c00a9249a2489decd679eb130547c68d318bf4f9
cathordivus <camilo.thorne@gmail.com>
2014-07-21 14:50:19 +0200

    adding file!

---
+++ ibm-dialog/my-dialog/test.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 21, 2014
+
+@author: camilothorne
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
